be4d0e92c1d9052d7e52e1ef049832d733657465
JFerrix <johnnyferrix@gmail.com>
2026-09-03 23:35:21 +0200

    Added rudimentary consent system

---
+++ Content.Shared/_Floof/Humanoid/Genitals/GenitalSystem.cs
@@ -0,0 +1,56 @@
+using Content.Shared._Common.Consent;
+using Content.Shared.Humanoid;
+using Content.Shared.Humanoid.Markings;
+using Content.Shared.Verbs;
+
+namespace Content.Shared._Floof.Humanoid.Genital;
+
+//Done:
+//Get Consent data for GenitalVisibility - DONE
+//Show/Hide Genitals based on consent switch - DONE
+//TODO:
+//Add verb functionality
+//Get events for filling and emptying the suit and oversuit slot and show/hide markings depending on if filled or not
+//Implement a way to hide/show specific and multiple markings
+
+public sealed class GenitalSystem : EntitySystem
+{
+    [Dependency] private readonly SharedConsentSystem _consent = default!;
+    [Dependency] private readonly SharedHumanoidAppearanceSystem _humanoidSystem = default!;
+    [Dependency] private readonly MarkingManager _markingManager = default!;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+        SubscribeLocalEvent<GenitalComponent, ComponentStartup>(OnStartup);
+        SubscribeLocalEvent<GenitalComponent, EntityConsentToggleUpdatedEvent>(OnConsentToggle);
+        SubscribeLocalEvent<GenitalComponent, GetVerbsEvent<Verb>>(AddGenitalVerb);
+    }
+    
+    private void OnStartup(EntityUid uid, GenitalComponent component, ComponentStartup args)
+    {
+        _humanoidSystem.SetLayerVisibility(uid, HumanoidVisualLayers.Genital, _consent.HasConsent(uid, "GenitalVisibility"));
+    }
+
+    private void OnConsentToggle(EntityUid uid, GenitalComponent component, EntityConsentToggleUpdatedEvent args)
+    {
+        //WHY THE FUCK DOES IT REQUIRE A BOOL INVERT BUT THE ONE ABOVE DOESNT?
+        _humanoidSystem.SetLayerVisibility(uid, HumanoidVisualLayers.Genital, !_consent.HasConsent(uid, "GenitalVisibility"));
+    }
+
+    //TODO: Do the verb things
+    private void AddGenitalVerb(EntityUid uid, GenitalComponent component, GetVerbsEvent<Verb> args)
+    {
+        //You cannot modify someone elses genitals
+        var isUser = args.User == args.Target;
+        if(!isUser) return;
+    }
+
+    public void SetMarkingVisibility(Entity<HumanoidAppearanceComponent> ent, string markingId, bool visible)
+    {
+        if (visible)
+            ent.Comp.HiddenMarkings.Remove(markingId);
+        else
+            ent.Comp.HiddenMarkings.Add(markingId);
+    }
+}
